9970937fa19cbca7d4aeef5e3c0eb2df7e493380
João - Windows <contato.jsbarbosa@gmail.com>
2026-01-24 23:01:11 -0300

    :construction: ci - Implementação inicial da configuração de injeção no .Infra

---
+++ src/payFlow.Infra/DependencyInjection/InfraModule.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using payFlow.Infra.Data;
+
+namespace payFlow.Infra.DependencyInjection
+{
+    public static class InfraModule
+    {
+
+        public static IServiceCollection AddInfra(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<PayFlowContext>(options => options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+            //services.AddScoped<ITransactionRepository, TransactionRepository>();
+            return services;
+
+        }
+    }
+}
