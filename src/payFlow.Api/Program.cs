ef979c0aee38be714386783109a8b1906547a746
João - Windows <contato.jsbarbosa@gmail.com>
2026-01-24 23:30:59 -0300

    :gear: [chore] - Configuração - Adicionado mapeamento de Middleware em Program

--- src/payFlow.Api/Program.cs
+++ src/payFlow.Api/Program.cs
@@ -1,4 +1,5 @@
 using FluentValidation;
+using payFlow.Api.Middlewares;
 using payFlow.Application.DTOs.Transactions.Requests;
 using payFlow.Infra.DependencyInjection;
 
@@ -9,7 +10,7 @@ builder.Services.AddInfra(builder.Configuration);
 builder.Services.AddValidatorsFromAssemblyContaining<TransactionCreate>();
 
 var app = builder.Build();
-
+app.UseMiddleware<ExceptionMiddleware>();
 app.MapGet("/", () => "Hello World!");
 
 app.Run();
