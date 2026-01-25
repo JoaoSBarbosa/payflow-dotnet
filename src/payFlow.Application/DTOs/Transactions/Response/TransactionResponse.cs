bff0f343bf15b21755f1abb23f0b63210f31a6b1
João - Windows <contato.jsbarbosa@gmail.com>
2026-01-24 23:32:14 -0300

    :package: [feat] - Padrão de request e response - Criação de padrão de response e request

---
+++ src/payFlow.Application/DTOs/Transactions/Response/TransactionResponse.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace payFlow.Application.DTOs.Transactions.Response
+{
+    internal class TransactionResponse
+    {
+    }
+}
