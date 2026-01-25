bf84f1915aff8dec5fccd50b0947448956c953cd
João - Windows <contato.jsbarbosa@gmail.com>
2026-01-24 22:55:04 -0300

    :sparkles: [feat] - Validação de dados de entrada - Transaction - Criado Validator com FluentValidator para Create request de Transaction - Adicionando mapeamento em .Api (program)

--- src/payFlow.Api/Program.cs
+++ src/payFlow.Api/Program.cs
@@ -1,4 +1,13 @@
+using FluentValidation;
+using payFlow.Application.DTOs.Transactions.Requests;
+using payFlow.Infra.DependencyInjection;
+
 var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddInfra(builder.Configuration);
+builder.Services.AddValidatorsFromAssemblyContaining<TransactionCreate>();
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
---
+++ src/payFlow.Application/DTOs/Transactions/Requests/TransactionCreate.cs
@@ -0,0 +1,6 @@
+﻿using payFlow.Core.Enums;
+
+namespace payFlow.Application.DTOs.Transactions.Requests
+{
+    public record TransactionCreate(string Title, long CategoryId, decimal Amount, ETransactionType Type){ }
+}
